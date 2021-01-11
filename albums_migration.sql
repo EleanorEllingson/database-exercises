fe7ce4d88ea524902b5145018f2e5fa9d9c37382
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-11 13:58:16 -0600

    exercise walkthrough

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
-use codeup_test_db;
+USE codeup_test_db;
 
 DROP TABLE if exists albums;
 
 CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist VARCHAR(100) NOT NULL,
-                        name VARCHAR(100) NOT NULL,
-                        release_date DATE NOT NULL,
-                        sales FLOAT,
-                        genre VARCHAR(250),
+                        artist VARCHAR(50) NOT NULL DEFAULT 'Unknown',
+                        name VARCHAR(50) NOT NULL default 'Untitled',
+                        release_date INT UNSIGNED,
+                        sales FLOAT UNSIGNED,
+                        genre VARCHAR(100),
                         PRIMARY KEY (id)
 );
