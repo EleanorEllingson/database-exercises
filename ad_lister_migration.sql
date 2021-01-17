7245a97b81f06f94f0e7fbfc313bd029bd3d4de4
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-16 21:05:43 -0600

    Queries for ad_lister Design

--- ad_lister_migration.sql
+++ ad_lister_migration.sql
@@ -26,3 +26,68 @@ CREATE TABLE ad_category (
      FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
+INSERT INTO users (email, password)
+VALUES
+('joe@gmail.com', 'password'),
+('ann@gmail.com', 'password'),
+('sarah@gmail.com', 'password');
+
+INSERT INTO ads (title, description, user_id)
+VALUES
+('Honda Accord', '2007 Honda Accord, 4 door, Standard transmission, runs well', 1),
+('Nintendo Switch', 'Switch game console', 2),
+('Boys size 10 clothes', '4 Old Navy Slim Jeans, 4 Old Navy tee-shirts', 3),
+('Kitchen table and Chairs', 'Dark wood rectangle table', 3);
+
+INSERT INTO categories (category)
+VALUES
+('Vehicles'),
+('Electronics'),
+('Clothing'),
+('Furniture');
+
+INSERT INTO ad_category (ad_id, category_id)
+VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
+
+# For a given ad, what is the email address of the user that created it?
+SELECT email
+FROM users
+WHERE id IN (
+    SELECT user_id
+    FROM ads
+    WHERE user_id = 1
+);
+
+# For a given ad, what category, or categories, does it belong to?
+SELECT category
+FROM categories
+WHERE id IN (
+    SELECT category_id
+    FROM ad_category
+    WHERE category_id = 1
+);
+
+# For a given category, show all the ads that are in that category.
+SELECT *
+FROM ads
+WHERE id IN (
+    SELECT ad_id
+    FROM ad_category
+    WHERE category_id = 2
+);
+
+# For a given user, show all the ads they have posted.
+SELECT *
+FROM ads
+WHERE user_id IN (
+    SELECT id
+    FROM users
+    WHERE id = 3
+);
+
+
+
